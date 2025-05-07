8209c1c412516108decc1fd54e106d1f383fdbd7
ashok <tamadaashokkumar4@gmial.com>
2025-05-07 05:41:12 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
-
 import './App.css'
 
 import Home from './Home'
-import Cart from './Cart'
 
-//write your code here
+// write your code here
 const App = () => (
   <>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/cart" component={Cart} />
-    </Switch>
+    <Home />
   </>
 )
 
